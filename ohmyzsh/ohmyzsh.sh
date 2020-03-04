6223b30d5f7f7161bdcd4afcf9999e3d080b6cbe
ohilho <iroiro515@gmail.com>
2020-03-05 00:37:09 +0900

    add lines for plugin install in ohmyzsh.sh

--- ohmyzsh/ohmyzsh.sh
+++ ohmyzsh/ohmyzsh.sh
@@ -1,2 +1,16 @@
+# install zsh
+sudo apt install zsh git -y
+
+# install oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Add plugins to the .zshrc file
+sed 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc > ~/.zshrc_new
+rm ~/.zshrc && mv ~/.zshrc_new ~/.zshrc
 
