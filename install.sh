8c508efe6161242947870db7f697de7ae4df2fc6
ohilho <iroiro515@gmail.com>
2021-05-23 22:17:36 +0900

    change url from feature branch to the master branch

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
-source <(curl -fsSL https://raw.githubusercontent.com/ohilho/initialize_script/feature/safe_sudo/common.sh)
+
+URL="https://raw.githubusercontent.com/ohilho/initialize_script/master"
+source <(curl -fsSL ${URL}/common.sh)
 
 # set defualt gitconfig
-curl_source("git/install_gitconfig.sh")
-# install oh-my-zsh with some plugins
-curl_source("ohmyzsh/ohmyzsh.sh")
-# install gtest
-curl_source("gtest/install_gtest.sh")
-# install gtest-runner
-curl_source("gtest/install_gtest-runner.sh")
+source <(curl -fsSL ${URL}/git/install_gitconfig.sh)
+# # install oh-my-zsh with some plugins
+source <(curl -fsSL ${URL}/ohmyzsh/ohmyzsh.sh)
+# # install gtest
+source <(curl -fsSL ${URL}/gtest/install_gtest.sh)
 
 #install custom tools
 git clone https://github.com/rise-lab-skku/src_generate_script.git "${HOME}/.custom_tools/psrcgen"
